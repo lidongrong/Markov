b8d6221fe22e931d2ec7bdeee673e724fe1b3ca6
lidongrong <32429197+lidongrong@users.noreply.github.com>
2022-04-18 18:48:26 +0800

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -4,26 +4,28 @@ Created on Wed Jan 19 18:12:56 2022
 
 @author: lidon
 """
-
 # test for normal distribution
 def p(x):
-  return 0.5*jnp.exp(-0.5*jnp.dot(x,x))+0.5*jnp.exp(-0.5*jnp.dot(x-jnp.array([3.,3.]),x-jnp.array([3.,3.])))
+  return 0.5*jnp.exp(-0.5*jnp.dot(x,x))+0.5*jnp.exp(-0.5*jnp.dot(x-jnp.array([5.,5.]),x-jnp.array([5.,5.])))
 
 
 def q(x,y):
   return jnp.exp(-0.5*jnp.dot(y-x,y-x))
 
-def q_sample(x):
-  I=np.eye(len(x))
-  y=stats.multivariate_normal.rvs(x,np.eye(len(x)),1)
+# For multiple chains, must specify the key to avoid generating same random numbers!
+def q_sample(x,key):
+  #np.random.seed()
+  y=jax.random.normal(key,x.shape)
+
+  y=x+y
   return y
 
-start_point=np.array([-1.,-1.])
+# Start Sampling
+start_point=np.random.normal(0,1,50)
+start_point=start_point.reshape((25,2))
+
+start_point=jnp.array(start_point)
 
 model=Sampler(p,q,q_sample)
-
-s=model.sampling(start_point,model.langevin,12000)
-
-import matplotlib.pyplot as plt
-
-plt.hist(s[3000:,0],bins=50)
+#model.para_q_sample(start_point)
+s=model.sampling(start_point,model.para_metropolis_kernel,20)
