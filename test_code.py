dd2bc64238332b8c973d0ff00bf657a74d3ba8eb
lidongrong <32429197+lidongrong@users.noreply.github.com>
2022-04-22 23:16:19 +0800

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -1,10 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 19 18:12:56 2022
-
-@author: lidon
-"""
-# test for normal distribution
+# test for mixed normal distribution
 def p(x):
   return 0.5*jnp.exp(-0.5*jnp.dot(x,x))+0.5*jnp.exp(-0.5*jnp.dot(x-jnp.array([5.,5.]),x-jnp.array([5.,5.])))
 
@@ -13,19 +7,20 @@ def q(x,y):
   return jnp.exp(-0.5*jnp.dot(y-x,y-x))
 
 # For multiple chains, must specify the key to avoid generating same random numbers!
-def q_sample(x,key):
-  #np.random.seed()
+def q_sample(x,key=jax.random.PRNGKey(int(time.time()))):
   y=jax.random.normal(key,x.shape)
-
   y=x+y
   return y
 
-# Start Sampling
-start_point=np.random.normal(0,1,50)
-start_point=start_point.reshape((25,2))
+
+# Run 20 Markov Chains in parallel
+start_point=np.random.normal(0,1,10)
+start_point=start_point.reshape((5,2))
 
 start_point=jnp.array(start_point)
 
+# Define the model
 model=Sampler(p,q,q_sample)
 #model.para_q_sample(start_point)
-s=model.sampling(start_point,model.para_metropolis_kernel,20)
+# run iterations, use the kernel specified
+s=model.sampling(start_point,model.para_langevin_kernel,5000)
